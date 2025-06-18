0664068c0b3a81d72857b911f0688c9a36872489
Marco <marco.vetrano01@gmail.com>
2025-06-18 15:18:05 +0200

    Imported QRC module in init file

--- QuantumStuff/__init__.py
+++ QuantumStuff/__init__.py
@@ -3,6 +3,7 @@ from .Operators import sigmax, sigmay, sigmaz, sigmam, sigmap, local_operators, 
 from .States import zero, one, plus, minus, right, left, random_qubit, BlochSpherePlot, bloch_vector
 from .Evolution import Lindblad_Propagator, Super_H, Super_D, dissipator, evolve_lindblad, evolve_unitary, Liouvillian, interaction, random_coupling
 from .Metrics import fidelity, trace_distance, von_neumann_entropy, mutual_info, Holevo_Info, purity
+from . import QRC
 
 __all__ = ["is_herm", "is_state", "dag", "is_norm", "tensor_product", "ptrace", "ket_to_dm",
            "sigmax", "sigmay", "sigmaz", "sigmam", "sigmap", "local_operators",
